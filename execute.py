b6b6a8390a4b644a447c1a8766d1f36f74ac2fc5
Jesther Jordan Minor <rejesth.main@gmail.com>
2025-02-20 13:10:15 +0800

    Migrate the training codes into separate repository

--- execute.py
+++ execute.py
@@ -1,9 +1,11 @@
 import torch
+import torchvision
 from ultralytics import YOLO
 
 
 def main():
     print("PyTorch version:", torch.__version__)
+    print("Torchvision version:", torchvision.__version__)
     print("CUDA available:", torch.cuda.is_available())
     print("CUDA version:", torch.version.cuda)
     print("Number of GPUs:", torch.cuda.device_count())
@@ -14,13 +16,12 @@ def main():
     else:
         print("GPU is not available. Training on CPU.")
         device = torch.device("cpu")
-
     return
     # Load the YOLO model
     model = YOLO("yolov8n.yaml")
 
     # Train the model
-    model.train(data="data.yaml", epochs=10, device=device)
+    model.train(data="data.yaml", epochs=500, device=device)
 
 
 if __name__ == "__main__":
